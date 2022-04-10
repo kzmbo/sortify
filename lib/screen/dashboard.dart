f70ed50e21bae64904784bf64e5e9d6ebc4e1066
Alex <alexkem132@gmail.com>
2022-04-09 19:47:06 -0700

    .

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -11,7 +11,7 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Container (
       child: Column(
         children: [
           //header
