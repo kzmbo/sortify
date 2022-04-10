ee83f60305516dfaf65e275c92181faeaec877e2
JFraire1 <Jose.Fraire@student.csulb.edu>
2022-04-09 20:10:32 -0700

    added functions for getting playlists and items

--- js/App.js
+++ js/App.js
@@ -1,20 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import 'package:js/js.dart';
 
 function App() {
-  print('hello')
   onPageLoad();
   return (
-      <button onClick = {() => requestAuth()}> try to authorize </button>
+  <button onClick={() => requestAuth()}> try to authorize </button>
   );
 }
 
+const get_playlist_items = "https://api.spotify.com/v1/playlists/";
+const get_playlists_uri = "https://api.spotify.com/v1/me/playlists"
 const redirect_uri = "http://localhost:3000";
 const client_id = "8405c732c9e44daf81d6a21cb5a0e8fc";
 const client_secret = "c33d9877c3a04803b131247f31644d23";
 const TOKEN = "https://accounts.spotify.com/api/token";
 
+var access_token;
+var refresh_token;
+
+//handles redirection back to homepage with spotify auth code
 function onPageLoad(){
   if (window.location.search.length > 0){
     handleRedirect();
@@ -26,7 +30,7 @@ function handleRedirect(){
   fetchAccessToken(code);
   window.history.pushState("", "", redirect_uri);
 }
-
+//parses spotify auth code
 function getCode(){
   let code = null;
   const queryString = window.location.search;
@@ -39,7 +43,7 @@ function getCode(){
   return code;
 }
 
-
+//gets access token using auth code
 function fetchAccessToken(code){
 
   let body = "grant_type=authorization_code";
@@ -50,7 +54,15 @@ function fetchAccessToken(code){
   callAuthorizationApi(body);
 
 }
-
+//refreshes token when expired
+function refreshAccessToken(){
+  refresh_token = localStorage.getItem("refresh_token");
+  let body = "grant_type=refresh_token";
+  body += "&refresh_token=" + refresh_token;
+  body += "&client_id=" + client_id;
+  callAuthorizationApi(body);
+}
+//calls for authorization
 function callAuthorizationApi(body){
 
   let xhr = new XMLHttpRequest();
@@ -60,17 +72,17 @@ function callAuthorizationApi(body){
   xhr.send(body);
   xhr.onload = handleAuthorizationResponse;
 }
-
+//handles response
 function handleAuthorizationResponse(){
   if (this.status == 200){
     var data = JSON.parse(this.responseText);
     console.log(data);
     if (data.access_token != undefined){
-      let access_token = data.access_token;
+      access_token = data.access_token;
       localStorage.setItem("access_token", access_token);
     }
     if (data.refresh_token != undefined){
-      let refresh_token = data.refresh_token;
+      refresh_token = data.refresh_token;
       localStorage.setItem("refresh_token", refresh_token);
     }
     onPageLoad();
@@ -80,7 +92,7 @@ function handleAuthorizationResponse(){
     alert(this.responseText);
   }
 }
-
+//initial request for authorization
 function requestAuth(){
   let url = "https://accounts.spotify.com/authorize";
   url += "?client_id=" + client_id;
@@ -90,9 +102,73 @@ function requestAuth(){
   url += "&scope=playlist-modify-public playlist-read-private playlist-modify-private";
   window.location.href = url;
 }
-
-function printH(){
-  print('hello')
+//general function for calling api
+function callApi(method, endpoint, body, responseHandler){
+  let url = endpoint;
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, url, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Authorization", "Bearer " + access_token);
+  xhr.send(body);
+  xhr.onload = responseHandler;
 }
+//gets playlists
+function getPlaylists(){
+  callApi("GET", get_playlists_uri + "?limit=1", null, handleGetPlaylistResponse);
+}
+//handles response for getplaylists
+function handleGetPlaylistResponse(){
+  if (this.status == 200){
+    var response = JSON.parse(this.responseText);
+    console.log(response);
+    let i = 0;
+    response.items.forEach(element=> {
+      //this part can change, is only a placeholder
+      //returns for each element returned 
+      ++i;
+      localStorage.setItem("id" + i, element.id);
+      localStorage.setItem("name" + i, element.name);
+    })
+    onPageLoad();
+  }
+  else if (this.status == 401){
+    refreshAccessToken();
+    onPageLoad();
+  }
+  else{
+    console.log(this.responseText);
+    alert(this.responseText);
+  }
+}
+//getsPlaylistItems
+function getPlaylistItems(playlist_id){
+  callApi("GET", get_playlist_items +
+      playlist_id + "/tracks?fields=items(track(id%2Cname%2Calbum(images)))",
+      null, playlistItemResponseHandler);
+}
+//handles above function
+function playlistItemResponseHandler(){
+  if (this.status == 200){
+    var response = JSON.parse(this.responseText);
+    console.log(response);
+    let i = 0;
+    response.items.forEach(element=> {
+      //more placeholder
+      ++i;
+      localStorage.setItem("songID" + i, element.track.id);
+      localStorage.setItem("songName" + i, element.track.name);
+    })
+    onPageLoad();
+  }
+  else if (this.status == 401) {
+    refreshAccessToken();
+    onPageLoad();
+  }
+  else {
+    console.log(this.responseText);
+    alert(this.responseText);
+  }
+}
+
 
 export default App;
