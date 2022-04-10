3529cc5dd3e25eea3a94096ab8867d55aa4932fd
immortal1999 <59622519+immortal1999@users.noreply.github.com>
2022-04-10 04:32:16 -0700

    more logic for keep,remove,new playlist

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         home: Scaffold(
-      body: LoginPage(), // Dashboard(),
+      body: LoginPage(),
     ));
   }
 }
