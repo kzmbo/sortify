551a2a0f4aca418121f4fe2f06f85a21aff8172e
MarkCarsonDev <64717642+MarkCarsonDev@users.noreply.github.com>
2022-04-10 10:06:24 -0700

    preparations for merging JS and Dart
    :D

--- lib/App.dart
+++ lib/App.dart
@@ -8,3 +8,18 @@ external void requestAuth();
 
 @JS('onPageLoad')
 external void onPageLoad();
+
+@JS('setCurrentPlaylist')
+external void setCurrentPlaylist(String playlistId);
+
+@JS('tempStorage')
+external dynamic temp;
+
+@JS('getUsername')
+external String getUsername();
+
+@JS('getProfilePicture')
+external String getProfilePicture();
+
+@JS('getPlaylists')
+external Map<String, dynamic> getPlaylists();
